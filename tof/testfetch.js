fc10ac8bac6f0e4d0e17f20996edf2894016d2bc
TzuEn Tsen <tsen1220.us@gmail.com>
2019-07-28 14:11:22 +0800

    'loaddes'

--- tof/testfetch.js
+++ tof/testfetch.js
@@ -22,22 +22,27 @@ const BDDATA= hh =>{
             aaaHTML=`<div>職業名稱:${classs.classname}</div><br/>`+
             `<div>職業類別:${classs.classtype}</div><br/>`+
             `<div>轉化後職業:${classs.convertclass}</div><br/>`+
-            `<div><img src="${classs.passive[0].p1img}">${classs.passive[0].p1}</div><br/>`+
+            `<div class='posit'>`+
+            `<div class='contain'><b>被動</b></div>`+
+            `<div><b><img src="${classs.passive[0].p1img}">${classs.passive[0].p1}</b></div>`+
             `<div>${classs.passive[0].p1description}</div><br/>`+
-            `<div><img src="${classs.passive[1].p2img}">${classs.passive[1].p2}</div><br/>`+
+            `<div><b><img src="${classs.passive[1].p2img}">${classs.passive[1].p2}</b></div><br/>`+
             `<div>${classs.passive[1].p2description}</div><br/>`+
+            `<div class='contain'><b>白天技能</b></div>`+
             `<div><img src="${classs.day[0].d1img}">${classs.day[0].d1}</div><br/>`+
             `<div>${classs.day[0].d1description}</div><br/>`+
             `<div>${classs.day[0].d1times}</div><br/>`+
-            `<div><img src="${classs.day[1].d2img}">${classs.day[1].d2}</div><br/>`+
+            `<div><b><img src="${classs.day[1].d2img}">${classs.day[1].d2}</b></div><br/>`+
             `<div>${classs.day[1].d2description}</div><br/>`+
             `<div>${classs.day[1].d2times}</div><br/>`           +
-            `<div><img src="${classs.night[0].n1img}">${classs.night[0].n1}</div><br/>`+
+            `<div class='contain'><b>晚上技能</b></div>` +
+            `<div><b><img src="${classs.night[0].n1img}">${classs.night[0].n1}</b></div><br/>`+
             `<div>${classs.night[0].n1description}</div><br/>`+
             `<div>${classs.night[0].n1times}</div><br/>`+
-            `<div><img src="${classs.night[1].n2img}">${classs.night[1].n2}</div><br/>`+
+            `<div><b><img src="${classs.night[1].n2img}">${classs.night[1].n2}</b></div><br/>`+
             `<div>${classs.night[1].n2description}</div><br/>`+
-            `<div>${classs.night[1].n2times}</div><br/>`
+            `<div>${classs.night[1].n2times}</div><br/>`+
+            `</div>`
      }
     })
     var a=document.getElementById('description')
