71a87d19eeff9bf75b01b7774ec4aa4ecbf9c395
TzuEn Tsen <tsen1220.us@gmail.com>
2019-07-29 15:36:05 +0800

    'revising'

--- tof/testfetch.js
+++ tof/testfetch.js
@@ -25,26 +25,26 @@ const BDDATA= hh =>{
             `</div>`+
             `<div class='posit'>`+
             `<div class='contain'><b>被動</b></div>`+
-            `<div><b><img src="${classs.passive[0].p1img}">${classs.passive[0].p1}</b></div>`+
+            `<div><b><img src="${classs.passive[0].p1img}" width="50px" height="50px">${classs.passive[0].p1}</b></div>`+
             `<div>${classs.passive[0].p1description}</div><br/>`+
-            `<div><b><img src="${classs.passive[1].p2img}">${classs.passive[1].p2}</b></div><br/>`+
+            `<div><b><img src="${classs.passive[1].p2img}" width="50px" height="50px">${classs.passive[1].p2}</b></div><br/>`+
             `<div>${classs.passive[1].p2description}</div><br/>`+
             `</div>`+
             `<div class='dposition'>`+
             `<div class='contain'><b>白天技能</b></div>`+
-            `<div><img src="${classs.day[0].d1img}">${classs.day[0].d1}</div><br/>`+
+            `<div><img src="${classs.day[0].d1img}" width="50px" height="50px">${classs.day[0].d1}</div><br/>`+
             `<div>${classs.day[0].d1description}</div><br/>`+
             `<div>${classs.day[0].d1times}</div><br/>`+
-            `<div><b><img src="${classs.day[1].d2img}">${classs.day[1].d2}</b></div><br/>`+
+            `<div><b><img src="${classs.day[1].d2img}" width="50px" height="50px">${classs.day[1].d2}</b></div><br/>`+
             `<div>${classs.day[1].d2description}</div><br/>`+
             `<div>${classs.day[1].d2times}</div><br/>`+
             `</div>`+
             `<div class="nposition">`+
             `<div class='contain'><b>晚上技能</b></div>` +
-            `<div><b><img src="${classs.night[0].n1img}">${classs.night[0].n1}</b></div><br/>`+
+            `<div><b><img src="${classs.night[0].n1img}" width="50px" height="50px">${classs.night[0].n1}</b></div><br/>`+
             `<div>${classs.night[0].n1description}</div><br/>`+
             `<div>${classs.night[0].n1times}</div><br/>`+
-            `<div><b><img src="${classs.night[1].n2img}">${classs.night[1].n2}</b></div><br/>`+
+            `<div><b><img src="${classs.night[1].n2img}" width="50px" height="50px">${classs.night[1].n2}</b></div><br/>`+
             `<div>${classs.night[1].n2description}</div><br/>`+
             `<div>${classs.night[1].n2times}</div><br/>`+
             `</div>`
@@ -70,6 +70,27 @@ function hide(num){
 
 function show(){
     for(var j=1;j<17;j++){
+    document.getElementById("bd"+j).style.width="184px";
+    document.getElementById("bd"+j).style.height="244px";
+    document.getElementById("bd"+j).style.display="inline";
+    }
+    var b=document.getElementById('description')
+    b.innerHTML=''
+ }
+
+
+ function show1(){
+    for(var j=1;j<13;j++){
+    document.getElementById("bd"+j).style.width="184px";
+    document.getElementById("bd"+j).style.height="244px";
+    document.getElementById("bd"+j).style.display="inline";
+    }
+    var b=document.getElementById('description')
+    b.innerHTML=''
+ }
+
+ function show2(){
+    for(var j=1;j<7;j++){
     document.getElementById("bd"+j).style.width="184px";
     document.getElementById("bd"+j).style.height="244px";
     document.getElementById("bd"+j).style.display="inline";
