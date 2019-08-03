89ca279da87b23ff095d26de5a860b9b535ea048
TzuEn Tsen <tsen1220.us@gmail.com>
2019-08-03 13:12:17 +0800

    weapon

--- tof/js/weapon.js
+++ tof/js/weapon.js
@@ -1,6 +1,12 @@
 var pic=[
-    "./tof/sass/avatar/1.png",
-    "./tof/sass/avatar/2.png",
+    "./tof/sass/weapon/1.png",
+    "./tof/sass/weapon/0.png",
+    "./tof/sass/weapon/2.png",
+    "./tof/sass/weapon/3.png",
+    "./tof/sass/weapon/4.png",
+    "./tof/sass/weapon/5.png",
+    "./tof/sass/weapon/6.png",
+    "./tof/sass/weapon/7.png",
     ]
     
     
@@ -8,8 +14,8 @@ var pic=[
         var h=`<h2 style="font-size:50px"><b>Weapon武器(金框為節慶限定)</b></h2>`;
         var img=``;
         var foot=` <div id="footer">© 2019 Made By Tzu-En ,who like this game. Images are from https://tol.fandom.com/wiki/</div>`
-        for(var i=0;i<2;i++){
-            img+=`<img src="${pic[i]}" >`
+        for(var i=0;i<8;i++){
+            img+=`<img src="${pic[i]}" width="700" height="400">`
         }
         document.getElementById('textcontainer').innerHTML= h+img+foot
     }
